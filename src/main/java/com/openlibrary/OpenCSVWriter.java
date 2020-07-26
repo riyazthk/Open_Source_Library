7f68f55f15ca04dbc0c57ff6a179b0cb61c66106
riyazthk <er.riyaz2507@gmail.com>
2020-07-26 19:13:08 +0530

    [Riyaz]ADDED:TO write a code convert csv file to json file

--- src/main/java/com/openlibrary/OpenCSVWriter.java
+++ src/main/java/com/openlibrary/OpenCSVWriter.java
@@ -1,24 +1,29 @@
 package com.openlibrary;
 
 
+import com.google.gson.Gson;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+
 import java.io.BufferedReader;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 
 public class OpenCSVWriter {
     public static void main(String[] arg) throws IOException {
-           File file=new File("E:\\BridgeLabz\\Intellj Projects\\Open_Source_Library\\object-list-sample.csv");
+        File file = new File("E:\\BridgeLabz\\Intellj Projects\\Open_Source_Library\\object-list-sample.csv");
+        File jsonfile = new File("E:\\BridgeLabz\\Intellj Projects\\Open_Source_Library\\object-list-sample.json");
+
         try {
-            FileWriter writer=new FileWriter(file);
+            FileWriter writer = new FileWriter(file);
             CSVWriter csvwriter = new CSVWriter(new FileWriter("E:\\BridgeLabz\\Intellj Projects\\Open_Source_Library\\object-list-sample.csv"));
             List<String[]> data = new ArrayList<>();
             data.add(new String[]{"Name", "email", "phno"});
@@ -33,15 +38,23 @@ public class OpenCSVWriter {
 
         try {
             Reader reader = Files.newBufferedReader(Paths.get("E:\\BridgeLabz\\Intellj Projects\\Open_Source_Library\\object-list-sample.csv"));
-            CsvToBean<CSVPojoClass> csvToBean = new CsvToBeanBuilder(reader)
-                    .withType(CSVPojoClass.class)
-                    .withIgnoreLeadingWhiteSpace(true)
-                    .build();
-
-            Iterator<CSVPojoClass> csviterator=csvToBean.iterator();
-            while(csviterator.hasNext()){
-                CSVPojoClass csvpojo=csviterator.next();
-                System.out.println(csvpojo.getName()+" "+csvpojo.getEmail()+" "+csvpojo.getPhNo());
+            CsvToBeanBuilder<CSVPojoClass> csvToBeanBuilder = new CsvToBeanBuilder(reader);
+            csvToBeanBuilder.withType(CSVPojoClass.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<CSVPojoClass> csvToBean = csvToBeanBuilder.build();
+            List<CSVPojoClass>csvPojoClasses=csvToBean.parse();
+            Gson gson=new Gson();
+            String json=gson.toJson(csvPojoClasses);
+            FileWriter jfile=new FileWriter(jsonfile);
+            jfile.write(json);
+            jfile.close();
+            Reader read = Files.newBufferedReader(Paths.get("E:\\BridgeLabz\\Intellj Projects\\Open_Source_Library\\object-list-sample.json"));
+            CSVPojoClass[] usr=gson.fromJson(read,CSVPojoClass[].class);
+            List<CSVPojoClass>csusr= Arrays.asList(usr);
+            Iterator<CSVPojoClass> csviterator = csusr.iterator();
+            while (csviterator.hasNext()) {
+                CSVPojoClass csvpojo = csviterator.next();
+                System.out.println(csvpojo.getName() + " " + csvpojo.getEmail() + " " + csvpojo.getPhNo());
             }
 
         } catch (IllegalStateException e) {
